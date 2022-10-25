8053f6971f9d27f6fd684ba4822b4f3148fdfc9d
Icaro de Barros <icarodebarros@gmail.com>
2022-10-25 09:48:28 -0300

    Adding example of integration test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
+/* Unit tests */
+
 test("on initial render, the pay button is disabled", async () => {
   render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
 
@@ -14,6 +16,20 @@ test("if an amount and note is entered, the pay button becomes enabled", async (
 
   // https://testing-library.com/docs/queries/about#priority
   // 1 - getByRole, 2 - getByLabelText, 3 - getByPlaceholderText, ...
+
+  const amountInput = screen.getByPlaceholderText(/amount/i);
+  userEvent.type(amountInput, "50");
+  const noteInput = screen.getByPlaceholderText(/add a note/i);
+  userEvent.type(noteInput, "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
+
+/* Integration test */
+test("pay button starts disabled but change when amount and note is entered", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 
   const amountInput = screen.getByPlaceholderText(/amount/i);
   userEvent.type(amountInput, "50");
